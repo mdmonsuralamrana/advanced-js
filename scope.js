df9eba7c65a680074ffe966d332a5cc3b55f9a66
mdmonsuralamrana <monsuralam7262@gmail.com>
2021-02-03 22:18:31 +0600

    scope

---
+++ scope.js
@@ -0,0 +1,12 @@
+function sum (num1 , num2){
+    let result = num1 + num2 ;
+    return result ;
+    if(result > 9){
+        let mood = "happy" ; 
+        mood = "sick";
+        mood = "tired"
+    }
+}
+
+const output = sum(7 , 3);
+console.log(output);
